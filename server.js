e8fd45c3b2b8a94278dc3f202d96d0c94c9e0c29
vardaro <anthonyavardaro@yahoo.com>
2018-02-14 01:50:16 -0600

    moltin

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const moltin = require('@moltin/sdk');
 
 const port = process.env.PORT || 9000;
+
+// set up middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
